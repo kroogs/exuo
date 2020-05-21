f5b0657efb7dbc057a1c2c2b59f17d1c722b290b
Ty Dira <ty@dira.dev>
2020-05-21 00:39:17 -0700

    Update author's name

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- * Copyright © 2020 Justin Krueger */
+ * Copyright © 2020 Ty Dira */
 
 import React from 'react'
 import { Link } from '@reach/router'
