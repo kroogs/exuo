79180b07a9429a90722a8f966b5cc9d2140cd139
Ty Dira <ty@dira.dev>
2020-05-26 19:21:02 -0700

    Slightly reorganize setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,8 @@
+import 'mobx-react-lite/batchingForReactDom'
 import '@testing-library/jest-dom/extend-expect'
 
 import { configure } from 'enzyme'
 import 'jest-enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-
-import 'mobx-react-lite/batchingForReactDom'
 
 configure({ adapter: new Adapter() })
