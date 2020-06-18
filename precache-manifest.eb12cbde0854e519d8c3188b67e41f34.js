4bd69873c023e6780b1a32d842007ea5f1b8b017
Ty Dira <ty@dira.dev>
2020-06-17 22:06:10 -0700

    Initial commit

---
+++ precache-manifest.eb12cbde0854e519d8c3188b67e41f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8705eced97fd8408fd332f70ac8be89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24130c1e583aad25049a",
+    "url": "/static/js/2.3e2b243e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.3e2b243e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb119675d0122046bf6",
+    "url": "/static/js/main.83f3601b.chunk.js"
+  },
+  {
+    "revision": "cd2fcc23c967109368ad",
+    "url": "/static/js/runtime-main.977720f5.js"
+  }
+]);
