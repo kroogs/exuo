645d46eed262a3fb8436f4fd076f725a130b77f7
Ty Dira <ty@dira.dev>
2020-08-16 13:38:39 -0700

    Add license comment to theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2020 Ty Dira <ty@dira.dev>
+
+ * This file is part of Exuo.
+
+ * Exuo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Exuo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exuo.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React from 'react'
 import {
   createMuiTheme,
