43bed846a84d6f168c5354b8bef8071b9fbb4e0c
Ty Dira <ty@dira.dev>
2020-10-21 16:56:30 -0700

    Update build

--- precache-manifest.dade5e4876cf40dce0490a3c9cc91ffc.js
+++ precache-manifest.dade5e4876cf40dce0490a3c9cc91ffc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62d9f3320498e8c504cdc440d7bd2cbd",
+    "revision": "cfe1b605c6dac4e2ed6303e6613f053d",
     "url": "/exuo/index.html"
   },
   {
-    "revision": "f61c76bf7edb6d8d4ca8",
+    "revision": "0d53707cb894181e79e1",
     "url": "/exuo/static/css/2.0949ee41.chunk.css"
   },
   {
-    "revision": "f61c76bf7edb6d8d4ca8",
-    "url": "/exuo/static/js/2.8b19b92b.chunk.js"
+    "revision": "0d53707cb894181e79e1",
+    "url": "/exuo/static/js/2.c1beac63.chunk.js"
   },
   {
     "revision": "a144e97867c790edf259731d9c7657f9",
-    "url": "/exuo/static/js/2.8b19b92b.chunk.js.LICENSE.txt"
+    "url": "/exuo/static/js/2.c1beac63.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "856975987e87a5f1ef84",
-    "url": "/exuo/static/js/main.da3b822d.chunk.js"
+    "revision": "5c599affea94f12a04bc",
+    "url": "/exuo/static/js/main.fc73ea22.chunk.js"
   },
   {
     "revision": "40b9bc05e28b6733503b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/exuo/precache-manifest.537575510010b0dbd5d8dd72042138bd.js"
+  "/exuo/precache-manifest.dade5e4876cf40dce0490a3c9cc91ffc.js"
 );
 
 self.addEventListener('message', (event) => {
